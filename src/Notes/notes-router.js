ca1f16a6eefa8810867132a0f31323fa818b6757
Linden Daniels <lindendaniels@gmail.com>
2019-11-23 11:48:41 -0500

    Update cors

--- src/Notes/notes-router.js
+++ src/Notes/notes-router.js
@@ -16,7 +16,7 @@ const serializeNote = note => ({
   content: xss(note.content),
 })
 app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "https://tranquil-shore-99373.herokuapp.com/"); // update to match the domain you will make the request from
+  res.header("Access-Control-Allow-Origin", "https://thawing-fjord-46211.herokuapp.com/api"); // update to match the domain you will make the request from
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
