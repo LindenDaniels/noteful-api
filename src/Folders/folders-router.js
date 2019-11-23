c784e849eb1638204c0b061ec611d4628e21814c
Linden Daniels <lindendaniels@gmail.com>
2019-11-23 11:54:37 -0500

    Update cors

--- src/Folders/folders-router.js
+++ src/Folders/folders-router.js
@@ -14,7 +14,7 @@ const serializeFolder = folder => ({
 })
 
 app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "https://tranquil-shore-99373.herokuapp.com/"); // update to match the domain you will make the request from
+  res.header("Access-Control-Allow-Origin", "https://tranquil-shore-99373.herokuapp.com/api"); // update to match the domain you will make the request from
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
--- src/app.js
+++ src/app.js
@@ -10,7 +10,7 @@ const FoldersRouter = require('./Folders/folders-router')
 
 const app = express()
 app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "https://tranquil-shore-99373.herokuapp.com/"); // update to match the domain you will make the request from
+  res.header("Access-Control-Allow-Origin", "https://tranquil-shore-99373.herokuapp.com/api"); // update to match the domain you will make the request from
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
